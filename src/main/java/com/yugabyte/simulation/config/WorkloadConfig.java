817c56f4edb49a5c290a63db67aee306ca69adea
timfaulkes <tfaulkes@yugabyte.com>
2022-09-14 16:59:25 -0600

    Initial load

--- src/main/java/com/yugabyte/simulation/config/WorkloadConfig.java
+++ src/main/java/com/yugabyte/simulation/config/WorkloadConfig.java
@@ -3,16 +3,11 @@ package com.yugabyte.simulation.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.yugabyte.simulation.service.AmexWorkload;
-import com.yugabyte.simulation.service.CapitalGroupWorkload;
-import com.yugabyte.simulation.service.CbsSportsWorkload;
 import com.yugabyte.simulation.service.GenericCassandraWorkload;
 import com.yugabyte.simulation.service.GenericWorkload;
 import com.yugabyte.simulation.service.NewFormatWorkload;
 import com.yugabyte.simulation.service.PitrSqlDemoWorkload;
 import com.yugabyte.simulation.service.SimpleSelectWorkload;
-import com.yugabyte.simulation.service.SonosWorkload;
-import com.yugabyte.simulation.service.TeslaWorkload;
 import com.yugabyte.simulation.service.WorkloadSimulation;
 
 @Configuration
@@ -41,34 +36,4 @@ public class WorkloadConfig {
     public WorkloadSimulation genericCassandraWorkload(){
         return new GenericCassandraWorkload();
     }
-
-
-    // ------------------------------------
-    // Customer workloads
-    @Bean(name="AmexWorkload")
-    public WorkloadSimulation amexWorkload(){
-        return new AmexWorkload();
-    }
-
-    @Bean(name="SonosWorkload")
-    public WorkloadSimulation sonosWorkload(){
-        return new SonosWorkload();
-    }
-
-    @Bean(name="TeslaWorkload")
-    public WorkloadSimulation teslaWorkload(){
-        return new TeslaWorkload();
-    }
-
-    @Bean(name="CapitalGroupWorkload")
-    public WorkloadSimulation capitalGroupWorkload(){
-        return new CapitalGroupWorkload();
-    }
-
-    @Bean(name="CbsSportsWorkload")
-    public WorkloadSimulation cbsSportsWorkload(){
-        return new CbsSportsWorkload();
-    }
-
-
 }
